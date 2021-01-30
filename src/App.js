d4fb9d4c4750c4e6ce91e4bdb0726e23f814f3e7
galanteria01 <shanuu12e@gmail.com>
2021-01-30 16:18:32 +0530

    Just a few more stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './files/components/Navbar';
 import Homepage from './files/pages/Homepage';
+import Developers from './files/pages/Developers';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage />
+      <Developers />
     </div>
   );
 }
--- src/files/pages/Developers.js
+++ src/files/pages/Developers.js
@@ -1,6 +1,53 @@
+import { Card, Button } from 'react-bootstrap';
+
 const Developers = () => {
     return ( 
-        <h1>Developers</h1>
+        <div className="developers container">
+            <div className="row">
+                <div className="col-sm-3">
+                    Developers
+                </div>
+                <div className="col-sm-3">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Vyom Desai</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Github</Button>
+                    </Card.Body>
+                </Card>
+                </div>
+                <div className="col-sm-3">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Rishawn Iyer</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Github</Button>
+                    </Card.Body>
+                </Card>
+                </div>
+                <div className="col-sm-3">
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Shoury Sharma</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Github</Button>
+                    </Card.Body>
+                </Card>
+                </div>
+            </div>
+        </div>
      );
 }
  
