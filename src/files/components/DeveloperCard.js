c9c634f35072cde2ae66aec85b52106fc5fd082f
CannedShroud <hypersync112@gmail.com>
2021-03-03 17:35:09 +0530

    webiste: Modularise the DeveloperCards

---
+++ src/files/components/DeveloperCard.js
@@ -0,0 +1,18 @@
+import { Card, Button } from 'react-bootstrap';
+
+const DeveloperCard = ({who, image, content,name}) => {
+    return <div className="col-sm-3">
+    <Card>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+            <Card.Title>{who}</Card.Title>
+            <p>{name}</p>
+            <Card.Text>
+                 {content}
+            </Card.Text>
+            <Button variant="primary" href={`https://github.com/${who}`}>Github</Button>
+        </Card.Body>
+    </Card>
+    </div>
+}
+export default DeveloperCard;
