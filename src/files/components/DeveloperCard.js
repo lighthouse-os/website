1503a1c5921e9d5dc931fdf7deb0fb9a307bad1d
CannedShroud <hypersync112@gmail.com>
2021-03-03 21:33:59 +0530

    website: We have a life

--- src/files/components/DeveloperCard.js
+++ src/files/components/DeveloperCard.js
@@ -1,15 +1,18 @@
 import { Card, Button } from 'react-bootstrap';
 
-const DeveloperCard = ({who, image, content,name}) => {
+const DeveloperCard = ({who, image, content,name,post}) => {
     return <div className="col-sm-3 devCard">
     <Card>
         <Card.Img variant="top" src={image} />
         <Card.Body>
             <Card.Title>{who}</Card.Title>
             <p>{name}</p>
-            <Card.Text>
-                 {content}
-            </Card.Text>
+                <div className="devCard__body">
+                    <h6>{post}</h6>
+                    <Card.Text>
+                        {content}
+                    </Card.Text>
+                </div>
             <Button variant="primary" href={`https://github.com/${who}`}>Github</Button>
         </Card.Body>
     </Card>
--- src/files/pages/Developers.js
+++ src/files/pages/Developers.js
@@ -4,12 +4,12 @@ const Developers = () => {
     return ( 
         <div className="developers container">
                 <div className="developers__title">
-                    <h1>Developers</h1>
+                    <h1>The Lighthouse Keepers</h1>
                 </div>
             <div className="devRow">
-                <DeveloperCard name="Vyom Desai" who="CannedShroud" image="https://avatars.githubusercontent.com/u/52405504?s=460&u=cbd8fdb7416c18f261b4109fc1b1672482c7ec0d&v=4" content="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-                <DeveloperCard name="Shourya Sharma" who="Galenteria01" image="https://avatars.githubusercontent.com/u/58800160?s=460&u=a4642596aca6664797bce5a52b7176fe90c9936a&v=4" content="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-                <DeveloperCard name="Rishwan Iyer" who="Stealth1226" image="https://avatars.githubusercontent.com/u/70256146?s=460&u=15eba7ed8610fd1842f4f3e6695dec080a1f87cd&v=4" content="ome quick example text to build on the card title and make up the bulk of the card's content."/>
+                <DeveloperCard name="Vyom Desai" who="CannedShroud" image="https://avatars.githubusercontent.com/u/52405504?s=460&u=cbd8fdb7416c18f261b4109fc1b1672482c7ec0d&v=4" post="Source Manager," content="The guy to spam for bugfixes."/>
+                <DeveloperCard name="Shourya Sharma" who="Galenteria01" image="https://avatars.githubusercontent.com/u/58800160?s=460&u=a4642596aca6664797bce5a52b7176fe90c9936a&v=4" post="Project Manager," content="Warm hugs only."/>
+                <DeveloperCard name="Rishawn Iyer" who="Stealth1226" image="https://avatars.githubusercontent.com/u/70256146?s=460&u=15eba7ed8610fd1842f4f3e6695dec080a1f87cd&v=4" post="Stealthy," content="The kid with insane ideas."/>
             </div>
         </div>
      );
